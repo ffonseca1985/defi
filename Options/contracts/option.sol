e4ef51f3640083abcc4f2a9e64fc68f5cf824301
Fabio R Fonseca <ffonseca1985@gmail.com>
2021-01-25 09:52:33 -0300

    add Currency

--- Options/contracts/option.sol
+++ Options/contracts/option.sol
@@ -3,21 +3,25 @@ pragma solidity >0.7.0 <0.9.0;
 
 struct Asset {
 
-        uint Value;
-        string Name;
-        string Description;
-        string Initials;
+        uint Value; // Give the valor of collateral
+        string Name; // Give the Name
+        string Description; // Give the description
+        string Initials; // Give the Initiais
  }
 
  struct Collateral {
-     Asset[] Assets;
-     uint CreateDate;
+     Asset[] Assets; // Assets of Collateral
+     uint CreateDate; // Date of CreateDate
  }  
 
  //Titular => has the rigth about the asset
  //Laucher => has of duty to buy
  enum Participant { Launcher, Titular }
+
  enum TypeParticipant { Put, Call}
+ 
+ //When init of transaction the sttatus is Open
+ //When finish of transaction the sttatus is Open
  enum StatusOption { Open, Finish }
 
 abstract contract OptionBase 
@@ -29,6 +33,11 @@ abstract contract OptionBase
     uint public MaturityDate;
     uint public AquisitionDate;
 
+    //Events
+    event AddPremium(address indexed _from, address indexed _to, uint256 _value);
+    event Init(address indexed _from, address indexed _to, uint256 _value);
+    event Finish(address indexed _from, address indexed _to, uint256 _value);
+
     Collateral[] public Collaterals;
     StatusOption public Status;
     TypeParticipant public Type;
@@ -39,7 +48,7 @@ abstract contract OptionBase
     /// @return if collateral has retired with sucess
     function RetireCollateral()  public  virtual returns (bool);
 
-    function Execute()  public  virtual;
+    function Execute()  public virtual;
 
     modifier HasCollateral {
         require(true, 'Without collateral to do transfer');
